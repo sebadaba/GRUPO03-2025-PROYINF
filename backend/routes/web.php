48804fd65a10aff8be634628ea47db0561aa2e8f
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-05-18 20:53:44 -0400

    api en routes/web.php temporal

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\AlumnoController;
+
+Route::apiResource('alumnos', AlumnoController::class);
+
+Route::get('/ping', function () {
+    return response()->json(['pong' => true]);
+});
